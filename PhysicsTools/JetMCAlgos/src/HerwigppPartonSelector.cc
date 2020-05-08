7791a1c8e20afc5386df7461fe166ad54ac7413a
Andrea Carlo Marini <andrea.marini@cern.ch>
2020-05-08 13:33:20 +0200

    fixing Parton selector for H7 ghost clustering.

--- PhysicsTools/JetMCAlgos/src/HerwigppPartonSelector.cc
+++ PhysicsTools/JetMCAlgos/src/HerwigppPartonSelector.cc
@@ -16,8 +16,8 @@ void HerwigppPartonSelector::run(const edm::Handle<reco::GenParticleCollection>&
                                  std::unique_ptr<reco::GenParticleRefVector>& partons) {
   // loop over particles and select partons
   for (reco::GenParticleCollection::const_iterator it = particles->begin(); it != particles->end(); ++it) {
-    if (it->status() != 2)
-      continue;  // only accept status==2 particles
+    if (it->status() != 2 and not (it->status() >=11 and it->status()<=200) )
+      continue;  // only accept status 2 and between 11--200 (see below for documentation)
     if (!CandMCTagUtils::isParton(*it))
       continue;  // skip particle if not a parton
 
@@ -26,3 +26,14 @@ void HerwigppPartonSelector::run(const edm::Handle<reco::GenParticleCollection>&
 
   return;
 }
+
+/* From the H7 documentation page: https://herwig.hepforge.org/tutorials/faq/general.html
+Since Herwig++ 2.4.0, we assign status codes according to this scheme:
+
+        0 : an empty entry, with no meaningful information and therefore to be skipped unconditionally
+        1 : a final-state particle, i.e. a particle that is not decayed further by the generator (may also include unstable particles that are to be decayed later, as part of the detector simulation). Such particles must always be labelled '1'.
+        2 : a decayed Standard Model hadron or tau or mu lepton, excepting virtual intermediate states thereof (i.e. the particle must undergo a normal decay, not e.g. a shower branching). Such particles must always be labelled '2'. No other particles can be labelled '2'.
+        3 : a documentation entry
+        4 : an incoming beam particle
+        11 - 200 : an intermediate (decayed/branched/...) particle that does not fulfill the criteria of status code 2, with a generator-dependent classification of its nature
+*/ 
